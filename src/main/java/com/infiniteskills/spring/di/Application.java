2c460591aaae9907c7aaec17efca0dc851f9b65f
Jeff Nadeau <jnadeau@gmail.com>
2015-09-05 19:08:04 -0400

    Another commit.

--- src/main/java/com/infiniteskills/spring/di/Application.java
+++ src/main/java/com/infiniteskills/spring/di/Application.java
@@ -2,13 +2,17 @@ package com.infiniteskills.spring.di;
 
 import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application {
 
 	private static final String TITLE = "Forest Gump";
 
 	public static void main(String[] args) {
-		
-		RentalService rentalService = new RentalService(new KioskLocator());
+		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml") ;
+		// RentalService rentalService = new RentalService(new KioskLocator());
+		RentalService rentalService = context.getBean("rentalService", RentalService.class) ;
 		List<RentalLocation> locations = rentalService.find(TITLE, "16802", 10);
 		
 		System.out.printf("%s is available at the following locations:\n", TITLE);
@@ -16,6 +20,9 @@ public class Application {
 		for(RentalLocation location:locations) {
 			System.out.println(location.getName());
 		}
+		
+		((ClassPathXmlApplicationContext)context).close() ;
+		
 		
 	}
 }
